92c849c5e8d98421dae6b88ec2f86aa07a47e296
Ian Schoenrock <Ian.Schoenrock@Transcendent.ai>
2019-01-30 13:05:52 -0500

    Disposed of Unused Code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.provider.Contacts
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
-import java.io.Serializable
-import java.net.URL
 
 
 private const val POST_DATA_API = "https://jsonplaceholder.typicode.com/posts"
@@ -18,21 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         GlobalScope.launch(Dispatchers.Main) {
             var postService = PostService()
 
             var postData = postService.fetchPostData(POST_DATA_API).await()
 
-            text_view_json.text = postData.toString()
+            text_view_json.text = postData
         }
-
-
-
-
     }
-
-
-
 }
 
--- app/src/main/java/com/example/myapplication/PostService.kt
+++ app/src/main/java/com/example/myapplication/PostService.kt
@@ -3,34 +3,14 @@ package com.example.myapplication
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
-import java.io.Serializable
 import java.net.URL
 
 class PostService {
-
-    object PostGenerator{
-        data class PostData(val id: String,
-                            val title: String,
-                            val body: String): Serializable
-
-        fun fromApiData(apiData: String): PostData{
-            //This is where you get the JSON object
-
-
-
-            val (id, title, body  ) = apiData.split(",")
-            return PostData(id, title, body)
-        }
-
-
-    }
-
     fun fetchPostData(Url: String): Deferred<String> {
 
         return GlobalScope.async {
             val apiData = URL(Url).readText()
             apiData
-
         }
     }
 }
